2834b819ab3bd62f13c3f810907f4fec4ff05f9c
ChuKhanhhh <chukhanhhh@chukhanhhh.local>
2021-06-26 12:15:41 +0700

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,34 +5,7 @@ import { Menu, Dropdown, message } from "antd";
 import { UserOutlined, DownOutlined } from "@ant-design/icons";
 const Header = ({ history, useLocalStorage }) => {
  
-  // const useLocalStorage = (key) => {
-  //   const [storedValue, setStoredValue] = useState(() => {
-  //     try {
-  //       const item = window.localStorage.getItem(key);
-  //       return item ? JSON.parse(item) : 0;
-  //     } catch (error) {
-        
-  //       console.log(error);
-  //       return 0;
-  //     }
-  //   });
-
-  //   const setValue = (value) => {
-  //     try {
-  //       // Allow value to be a function so we have same API as useState
-  //       const valueToStore =
-  //         value instanceof Function ? value(storedValue) : value;
-  //       // Save state
-  //       setStoredValue(valueToStore);
-  //       // Save to local storage
-  //       window.localStorage.setItem(key, JSON.stringify(valueToStore));
-  //     } catch (error) {
-  //       // A more advanced implementation would handle the error case
-  //       console.log(error);
-  //     }
-  //   };
-  //   return [storedValue, setValue];
-  // }
+ 
 
   const userClick = ({ key }) => {
     switch (key) {
